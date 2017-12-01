fa58a80b7423230b3c2c48387d2152525867763a
Brablawaxplagabrwabla <32771607+Brablawaxplagabrwabla@users.noreply.github.com>
2017-12-01 14:25:14 -0400

    Update MedicamentoController.js

--- api/controllers/MedicamentoController.js
+++ api/controllers/MedicamentoController.js
@@ -63,6 +63,7 @@ module.exports = {
     var aux = req.param('id');
     Medicamento.query("select medicamento.idMedicamento, medicamento.NomComercial from medicamento"+
     "inner join (select Medicamento_idMedicamento from compactivos where CompActivos like concat('%','"+aux+"','%')) as tablita on medicamento.idMedicamento = tablita.Medicamento_idMedicamento", function(err, result){
+      sails.log(result);
       if (err) sails.log(err);
       if (result) {
         aux = JSON.parse(JSON.stringify(result));
